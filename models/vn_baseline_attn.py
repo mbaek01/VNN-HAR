184490e1cb74fa2ccf74a1986e0b32f165ee53ee
mbaek01 <marcusbaek248@gmail.com>
2025-05-29 09:00:51 +0000

    fix: vn_inv_attn mean concat added

--- models/vn_baseline_attn.py
+++ models/vn_baseline_attn.py
@@ -19,8 +19,8 @@ class VN_Inv_Baseline_Attn(nn.Module):
         self.vn_act = VNLeakyReLU(self.nb_units, share_nonlinearity=False, negative_slope=0.0)
 
         # Self-Attention Encoder
-        self.VNInvEncoderLayer1 = VNInvEncoderLayer(d_model = self.nb_units, n_heads=4 , d_ff = self.nb_units*4)
-        self.VNInvEncoderLayer2 = VNInvEncoderLayer(d_model = self.nb_units, n_heads=4 , d_ff = self.nb_units*4)
+        self.VNInvEncoderLayer1 = VNInvEncoderLayer(d_model = self.nb_units, n_heads=4 , d_ff = self.nb_units*4*2)
+        self.VNInvEncoderLayer2 = VNInvEncoderLayer(d_model = self.nb_units, n_heads=4 , d_ff = self.nb_units*4*2)
 
         # Global Temporal Attention
         self.VNInvAttentionWithContext = VNInvAttentionWithContext(self.nb_units)
@@ -29,7 +29,7 @@ class VN_Inv_Baseline_Attn(nn.Module):
         self.std_feature = VNStdFeature(self.nb_units, dim=3, normalize_frame=False)
 
         # MLP Classifier
-        self.fc2 = nn.Linear(self.nb_units*3, 4*nb_classes)
+        self.fc2 = nn.Linear(3*self.nb_units, 4*nb_classes)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=0.2)
         self.fc_out = nn.Linear(4*nb_classes, nb_classes)
@@ -53,10 +53,10 @@ class VN_Inv_Baseline_Attn(nn.Module):
         batch_temp = x.size(0)
         # x_mean = x.mean(dim=-1, keepdim=True).expand(x.size())
         # x = torch.cat((x, x_mean), 2)
-        x, _ = self.std_feature(x)                             # (B, D, 3)
+        x, _ = self.std_feature(x)                          # (B, D, 3)
 
         # MLP Classifier
-        x = x.view(batch_temp, -1)                             # (B, D*3)
+        x = x.view(batch_temp, -1)                          # (B, 3*D)
         
         x = self.dropout(self.relu(self.fc2(x)))            # (B, 4*N); N = num classes
 
--- models/vn_inv_attn.py
+++ models/vn_inv_attn.py
@@ -10,38 +10,48 @@ class VNInvAttentionLayer(nn.Module):
         super(VNInvAttentionLayer, self).__init__()
 
         # bias=False for VNLinear
-        self.query = VNLinear(d_model, d_model) 
-        self.key = VNLinear(d_model, d_model)
-        self.value = VNLinear(d_model, d_model)
-        self.out = VNLinear(d_model, d_model)
+        self.query = VNLinear(d_model, d_model)                                                     
+        self.key = VNLinear(d_model, d_model)                                    
+        self.value = VNLinear(d_model, d_model)                                 
+        self.out = VNLinear(2*d_model, d_model)
 
         self.n_heads = n_heads
 
-        self.q_std_feature = VNStdFeature(d_model//n_heads, dim=5, normalize_frame=False)
-        self.k_std_feature = VNStdFeature(d_model//n_heads, dim=5, normalize_frame=False)
-        self.v_std_feature = VNStdFeature(d_model//n_heads, dim=5, normalize_frame=False)
+        self.q_std_feature = VNStdFeature(2*d_model//n_heads, dim=5, normalize_frame=False)
+        self.k_std_feature = VNStdFeature(2*d_model//n_heads, dim=5, normalize_frame=False)
+        self.v_std_feature = VNStdFeature(2*d_model//n_heads, dim=5, normalize_frame=False)
 
     def forward(self, queries, keys, values):
         B, L, _, _ = queries.shape 
         H = self.n_heads
 
         queries = self.query(queries).view(B, L, 3, H, -1)                      # (B, L, 3, H, D)
-        keys = self.key(keys).view(B, L, 3, H, -1)
-        values = self.value(values).view(B, L, 3, H, -1)
+        keys = self.key(keys).view(B, L, 3, H, -1)                              # (B, L, 3, H, D) 
+        values = self.value(values).view(B, L, 3, H, -1)                        # (B, L, 3, H, D) 
 
         # Invariant query, key, values
-        queries_inv, _ = self.q_std_feature(queries)                               # (B, L, 3, H, D)
-        keys_inv, _ = self.k_std_feature(keys)
-        values_inv, _ = self.v_std_feature(values) 
+        queries_mean = queries.mean(dim=1, keepdim=True).expand(queries.size())
+        queries = torch.cat((queries, queries_mean), -1)
 
+        keys_mean = keys.mean(dim=1, keepdim=True).expand(keys.size())
+        keys = torch.cat((keys, keys_mean), -1)         
+
+        values_mean = values.mean(dim=1, keepdim=True).expand(values.size())
+        values = torch.cat((values, values_mean), -1)                                                   
+
+        queries_inv, _ = self.q_std_feature(queries)                            # (B, L, 3, H, 2*D)
+        keys_inv, _ = self.k_std_feature(keys)                                  # (B, L, 3, H, 2*D)
+        values_inv, _ = self.v_std_feature(values)                              # (B, L, 3, H, 2*D)
+
+        # Attention Score Applied
         score = torch.einsum("blvhd, bsvhd->bvhls", queries_inv, keys_inv)      # (B, 3, H, L, L)
         _, _, _, _, D = queries_inv.shape
         scale = 1./math.sqrt(D)
         Attn = torch.softmax(scale * score, dim=-1)
-        V = torch.einsum("bvhls,bsvhd->blvhd", Attn, values_inv).contiguous()   # (B, L, 3, H, D)
+        V = torch.einsum("bvhls,bsvhd->blvhd", Attn, values_inv).contiguous()   # (B, L, 3, H, 2*D)
 
         # Merge heads
-        out = V.view(B, L, 3, -1)                                               # (B, L, 3, D)
+        out = V.view(B, L, 3, -1)                                               # (B, L, 3, 2*D)
         out = self.out(out)
         return out, Attn
     
@@ -70,11 +80,13 @@ class VNInvEncoderLayer(nn.Module):
         attn_output, attn = self.attention( x, x, x )
         attn_output = self.dropout1(attn_output)             # (B, L, 3, D)
 
+        # Residual Connection with LayerNorm
         out1  = self.layernorm1(x + attn_output)
 
         ffn_output = self.ffn2(self.vn_relu(self.ffn1(out1)))
         ffn_output = self.dropout2(ffn_output)               # (B, L, 3, D)
 
+        # Residual Connection with LayerNorm
         out2 = self.layernorm2(out1 + ffn_output)            # (B, L, 3, D)
 
         return out2 
@@ -94,9 +106,9 @@ class VNInvAttentionWithContext(nn.Module):
             raise NotImplementedError
         
         # Invariant Layer for attn_weights
-        self.attn_std_feature = VNStdFeature(hidden_dim, dim=4, normalize_frame=False)
+        self.attn_std_feature = VNStdFeature(2*hidden_dim, dim=4, normalize_frame=False)
         
-        self.fc2 = VNLinear(hidden_dim, 1)
+        self.fc2 = VNLinear(2*hidden_dim, 1)
 
     def forward(self, x):
         # context = x[:, :-1, :]
@@ -106,12 +118,17 @@ class VNInvAttentionWithContext(nn.Module):
         '''
         uit = self.activation(self.fc1(x))                                          # (B, L, 3, D)
 
-        inv_uit, _ = self.attn_std_feature(uit)                                     # (B, L, 3, D)
+        # Invariant Layer 
+        uit_mean = uit.mean(dim=1, keepdim=True).expand(uit.size())
+        uit = torch.cat((uit, uit_mean), -1) 
+
+        inv_uit, _ = self.attn_std_feature(uit)                                     # (B, L, 3, 2*D)
        
         ait = self.fc2(inv_uit)                                                     # (B, L, 3, 1)
        
+        # Applying Attention Weight 
         attn_weights = F.softmax(ait, dim=1).transpose(-1, -3)                      # (B, 1, 3, L)
 
-        out = torch.einsum('bsik,bkin->bin', attn_weights, x).squeeze(-2) # + last  # (B, 3, C)
+        out = torch.einsum('bsik,bkin->bin', attn_weights, x).squeeze(-2) # + last  # (B, 3, D)
 
         return out
