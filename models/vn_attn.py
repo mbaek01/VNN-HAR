fix: rotation on vn_baseline_attn

models/vn_attn.py
@@ -116,7 +116,7 @@ class VNAttentionWithContext(nn.Module):
         else:
             raise NotImplementedError
         
-        self.fc2 = VNLinear(hidden_dim, 1)
+        self.fc2 = nn.Linear(hidden_dim, 1)
 
     def forward(self, x):
         # context = x[:, :-1, :]

models/vn_baseline_attn.py
@@ -26,10 +26,10 @@ class VN_Inv_Baseline_Attn(nn.Module):
         self.VNInvAttentionWithContext = VNInvAttentionWithContext(self.nb_units)
 
         # Final Invariant Layer
-        self.std_feature = VNStdFeature(2*self.nb_units, dim=3, normalize_frame=False)
+        self.std_feature = VNStdFeature(self.nb_units, dim=3, normalize_frame=False)
 
         # MLP Classifier
-        self.fc2 = nn.Linear(2*3*self.nb_units, 4*nb_classes)
+        self.fc2 = nn.Linear(3*self.nb_units, 4*nb_classes)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=0.2)
         self.fc_out = nn.Linear(4*nb_classes, nb_classes)
@@ -51,12 +51,12 @@ class VN_Inv_Baseline_Attn(nn.Module):
 
         # Final Invariant Layer
         batch_temp = x.size(0)
-        x_mean = x.mean(dim=-1, keepdim=True).expand(x.size())
-        x = torch.cat((x, x_mean), 2)
-        x, _ = self.std_feature(x)                          # (B, 2*D, 3)
+        # x_mean = x.mean(dim=-1, keepdim=True).expand(x.size())
+        # x = torch.cat((x, x_mean), 2)
+        x, _ = self.std_feature(x)                          # (B, D, 3)
 
         # MLP Classifier
-        x = x.view(batch_temp, -1)                          # (B, 2*3*D)
+        x = x.view(batch_temp, -1)                          # (B, 3*D)
         
         x = self.dropout(self.relu(self.fc2(x)))            # (B, 4*N); N = num classes
 

train/rotation.py
@@ -21,9 +21,9 @@ Device = Union[str, torch.device]
 def rotation(batch_x1, rot, device):
     # batch_x1: (B, 1, L, C)
     time_length = batch_x1.size(2)
-    batch_x1 = vn_c_reshape(batch_x1, time_length) # (B, L, C) -> (B, L, 3, C//3)
+    batch_x1 = vn_c_reshape(batch_x1, time_length).squeeze(1)           # (B, 1, L, C) -> (B, L, 3, C//3)
 
-    batch_x1 = batch_x1.transpose(-2, -1) # (B, L, C//3, 3)
+    batch_x1 = batch_x1.transpose(-2, -1)                               # (B, L, C//3, 3)
 
     trot = None
 
@@ -38,8 +38,8 @@ def rotation(batch_x1, rot, device):
         batch_x1_rot = trot.transform_points(batch_x1)
 
     # back to original input shape 
-    batch_x1_rot = batch_x1_rot.flatten(start_dim=-2) # (B, L, C)
-    batch_x1_rot = batch_x1_rot.unsqueeze(1) # (B, 1, L ,C) - original shape
+    batch_x1_rot = batch_x1_rot.transpose(-1,-2).flatten(start_dim=-2) # (B, L, C)
+    batch_x1_rot = batch_x1_rot.unsqueeze(1)                           # (B, 1, L ,C) - original shape
 
     return batch_x1_rot
 
@@ -151,7 +151,7 @@ class Rotate:
         # points_out = self._broadcast_bmm(points_batch, self.rotation) # (N, P, 3) @ (N, 3, 3)
 
         B, L, C, _ = points_batch.shape
-        points_batch = points_batch.reshape(B, -1, 3)
+        points_batch = points_batch.reshape(B, L*C, 3)      # (B, L*C, 3)
 
         points_out = torch.bmm(points_batch, self.rotation)
         points_out = points_out.view(B, L, C, 3)